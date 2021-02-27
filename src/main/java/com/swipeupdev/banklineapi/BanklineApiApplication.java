c8cc740014bd9b300a28354c249744bbd9dfc66b
Thomas M <twsm000@gmail.com>
2021-02-27 15:18:51 -0300

    Repo inicializado

---
+++ src/main/java/com/swipeupdev/banklineapi/BanklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.swipeupdev.banklineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swipeupdev/banklineapi/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swipeupdev.banklineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
