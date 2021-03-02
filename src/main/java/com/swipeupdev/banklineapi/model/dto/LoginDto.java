1eefa0264c76a0a40168f4b8a4ad1d66304c803a
Thomas M <twsm000@gmail.com>
2021-03-02 18:39:58 -0300

    Atualização model.LoginDto

--- src/main/java/com/swipeupdev/banklineapi/model/dto/LoginDto.java
+++ src/main/java/com/swipeupdev/banklineapi/model/dto/LoginDto.java
@@ -1,4 +1,22 @@
 package com.swipeupdev.banklineapi.model.dto;
 
 public class LoginDto {
+    private String login;
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
