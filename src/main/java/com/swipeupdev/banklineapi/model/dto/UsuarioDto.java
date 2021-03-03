94d23e7238c1556f791dc669a3a6a77b66ab2835
Thomas M <twsm000@gmail.com>
2021-03-03 02:50:35 -0300

    Atualização validações mode.dto.UsuarioDto

--- src/main/java/com/swipeupdev/banklineapi/model/dto/UsuarioDto.java
+++ src/main/java/com/swipeupdev/banklineapi/model/dto/UsuarioDto.java
@@ -6,20 +6,21 @@ import javax.validation.constraints.Size;
 
 public class UsuarioDto {
 
-    @NotBlank(message = "O nome de usuário não pode ser em branco.")
-    @Size(min = 6, max = 100, message = "O nome do usuário deverá ser entre 6 e 100 caracteres.")
+    @NotBlank(message = "O 'nome' do usuário não pode ser em branco.")
+    @Size(min = 6, max = 100, message = "O 'nome' do usuário deverá ser entre 6 e 100 caracteres.")
     private String nome;
 
-    @NotBlank(message = "O login de usuário não pode ser em branco.")
-    @Size(min = 4, max = 20, message = "O login de usuário deverá ser entre 4 e 20 caracteres.")
+    @NotBlank(message = "O 'login' do usuário não pode ser em branco.")
+    @Size(min = 4, max = 20, message = "O 'login' de usuário deverá ser entre 4 e 20 caracteres.")
     private String login;
 
-    @NotBlank(message = "O cpf do usuário não pode ser em branco.")
-    @Pattern(regexp = "[\\d]{11}", message = "Digite apenas números.")
+    @NotBlank(message = "O 'cpf' do usuário não pode ser em branco.")
+    @Size(min = 11, max = 11, message = "O 'cpf' deve conter apenas 11 números.")
+    @Pattern(regexp = "[\\d]{11}", message = "O 'cpf' deve conter apenas caracteres numéricos.")
     private String cpf;
 
-    @NotBlank(message = "A senha de suário não pode ser em branco.")
-    @Size(min = 8)
+    @NotBlank(message = "A 'senha' do usuário não pode ser em branco.")
+    @Size(min = 8, message = "A 'senha' do usuário deve ser maior igual a 8 caracteres.")
     private String senha;
 
     public String getNome() {
