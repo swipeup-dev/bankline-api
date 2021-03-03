98839082994c7684feccaa2022512193404f5a4d
Thomas M <twsm000@gmail.com>
2021-03-03 13:55:17 -0300

    Adição/Atualização controller.exceptions.handler

--- src/main/java/com/swipeupdev/banklineapi/controller/exceptions/handler/HttpMessageNotReadableExceptionHandler.java
+++ src/main/java/com/swipeupdev/banklineapi/controller/exceptions/handler/HttpMessageNotReadableExceptionHandler.java
@@ -3,17 +3,17 @@ package com.swipeupdev.banklineapi.controller.exceptions.handler;
 import com.swipeupdev.banklineapi.controller.exceptions.StandardError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class PersistenceExceptionHandler extends HandleException<PersistenceException> {
+public class HttpMessageNotReadableExceptionHandler extends HandleException<HttpMessageNotReadableException>{
     @Override
-    @ExceptionHandler(PersistenceException.class)
-    public ResponseEntity<StandardError> handler(PersistenceException e, HttpServletRequest request) {
-        return this.handler(e, request, "Ocorreu uma falha ao persistir os dados.", HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> handler(HttpMessageNotReadableException e, HttpServletRequest request) {
+        return this.handler(e, request, "JSON inválido", HttpStatus.BAD_REQUEST);
     }
 }
