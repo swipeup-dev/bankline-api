72cff930d5f6fc749e62c219d85ebf55dffc2652
Thomas M <twsm000@gmail.com>
2021-03-03 02:53:16 -0300

    Atualização de dependências
     - Desabilitando Spring SecurityAytiConfiguration

--- src/main/java/com/swipeupdev/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/swipeupdev/banklineapi/BanklineApiApplication.java
@@ -2,8 +2,9 @@ package com.swipeupdev.banklineapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
