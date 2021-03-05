1fa1b73daf0c9eeaff5b1816b6c84bdfe36f304b
Thomas M <twsm000@gmail.com>
2021-03-05 15:14:30 -0300

    Hotfix warnings Serializeble/version

--- src/main/java/com/swipeupdev/banklineapi/model/exception/RecordNotFoundException.java
+++ src/main/java/com/swipeupdev/banklineapi/model/exception/RecordNotFoundException.java
@@ -1,6 +1,8 @@
 package com.swipeupdev.banklineapi.model.exception;
 
 public class RecordNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = -1934235367882618110L;
+
     public RecordNotFoundException(String message) {
         super(message);
     }
