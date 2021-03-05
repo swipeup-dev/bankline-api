be680772ad326a2ac349a7d9a0266af177b52079
Thomas M <twsm000@gmail.com>
2021-03-05 15:14:30 -0300

    Hotfix warnings Serializeble/version

--- src/main/java/com/swipeupdev/banklineapi/model/exception/EntityRequirementException.java
+++ src/main/java/com/swipeupdev/banklineapi/model/exception/EntityRequirementException.java
@@ -1,6 +1,8 @@
 package com.swipeupdev.banklineapi.model.exception;
 
 public class EntityRequirementException extends RuntimeException {
+    private static final long serialVersionUID = 5491793674767327337L;
+
     public EntityRequirementException(String message) {
         super(message);
     }
